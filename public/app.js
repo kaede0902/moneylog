7e06a13daeb155c9440db2798dd9876f1261ae64
kaede0902 <kaede0920js@gmail.com>
2020-03-15 11:40:12 +0900

    end CRUD's R

--- public/app.js
+++ public/app.js
@@ -2,31 +2,40 @@ document.addEventListener('DOMContentLoaded', event => {
         let app = firebase.app();
         let db = firebase.firestore();
 
+        let num = 0;
         db.collection("items").get().
         then(function(querySnapshot) {
             querySnapshot.forEach(function(doc) {
                 let data = doc.data();
-                console.log('id: ', doc.id, ": ", doc.data());
+                console.log('id: ', doc.id, 
+                    ": ", doc.data());
+                $('#usercol').append(
+                    `
+                        <tr>
+                        <th scope="row">${num++}</th>
+                        <td> ${data.name} </td>
+                        <td> ${data.price} </td>
+                        <td> ${data.purpose} </td>
+                        </tr>
+                    `
+                );
+
             });
         });
         // here jq click, does not work
-      });
-
-function OnButtonClick() {
-    let app = firebase.app();
-    let db = firebase.firestore();
-    db.collection("items").add({
-        name: 'added by',
-        price: 0314,
-        purpose: 'browser',
+    $('.submit').on('click', function(e){
+        e.preventDefault();
+        console.log('loaded');
+        let app = firebase.app();
+        let db = firebase.firestore();
+        let name = $('#name').val();
+        let purpose = $('#purpose').val();
+        let price = $('#price').val();
+        addData(name,price,purpose);
     })
-}
+});
 
-$("button").click(function(){
-    let name = $('#name').val();
-    let purpose = $('#purpose').val();
-    let price = $('#price').val();
-    alert(`${name}, ${purpose}, ${price}`);
+function addData(name,price,purpose) {
     let app = firebase.app();
     let db = firebase.firestore();
     db.collection("items").add({
@@ -34,10 +43,9 @@ $("button").click(function(){
         price: price,
         purpose: purpose,
     })
-    .then(function(docRef) {
-        console.log("Document written with ID: ", docRef.id);
-    })
-    .catch(function(error) {
-        console.error("Error adding document: ", error);
-    });
-}); 
+}
+
+
+
+
+

fdc0cd3cd21db30f4bcd73a3bbcc274c8bd5be3f
kaede0902 <kaede0920js@gmail.com>
2020-03-15 17:07:39 +0900

    defend XSS INJECTION

--- public/app.js
+++ public/app.js
@@ -1,27 +1,40 @@
 document.addEventListener('DOMContentLoaded', event => {
-        let app = firebase.app();
-        let db = firebase.firestore();
+  let app = firebase.app();
+  let db = firebase.firestore();
 
-        let num = 0;
-        db.collection("items").get().
-        then(function(querySnapshot) {
-            querySnapshot.forEach(function(doc) {
-                let data = doc.data();
-                console.log('id: ', doc.id, 
-                    ": ", doc.data());
-                $('#usercol').append(
-                    `
-                        <tr>
-                        <th scope="row">${num++}</th>
-                        <td> ${data.name} </td>
-                        <td> ${data.price} </td>
-                        <td> ${data.purpose} </td>
-                        </tr>
-                    `
-                );
+  let num = 0;
+  db.collection("items").get().
+    then(function(querySnapshot) {
 
-            });
-        });
+    function escapeHtml(unsafe) {
+      console.log(unsafe);
+        return unsafe
+         .replace('&', '&amp;')
+         .replace("'", '&#x27;')
+         .replace('`', '&#x60;')
+         .replace('"', '&quot;')
+         .replace('<', '&lt;')
+         .replace('>', '&gt;')
+    }
+      querySnapshot.forEach(function(doc) {
+        let data = doc.data();
+        for (let key in data) {
+          if (typeof(data[key]) === 'string')
+           data[key] = escapeHtml(data[key]);
+        }
+        console.log('id: ', doc.id, ": ", doc.data());
+        $('#usercol').append(
+          `
+            <tr>
+            <th scope="row">${num++}</th>
+            <td> ${data.name} </td>
+            <td> ${data.price} </td>
+            <td> ${data.purpose} </td>
+            </tr>
+          `
+        );
+      });
+    });
         // here jq click, does not work
     $('.submit').on('click', function(e){
         e.preventDefault();
@@ -44,7 +57,6 @@ function addData(name,price,purpose) {
         purpose: purpose,
     })
 }
-
 
 
 
